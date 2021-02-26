d02553fb056d63bc918a97dbdd220116df060684
Josh Bonfield <josh@grizzlyware.com>
2021-02-26 14:51:22 +0000

    chore: tidy up test

--- tests/Resources/VirtualMachinePackagesTest.php
+++ tests/Resources/VirtualMachinePackagesTest.php
@@ -2,8 +2,6 @@
 
 namespace Krystal\Katapult\Tests\Resources;
 
-use Krystal\Katapult\Resources\DataCenter;
-use Krystal\Katapult\Resources\Organization;
 use Krystal\Katapult\Resources\VirtualMachinePackage;
 use Krystal\Katapult\Tests\Concerns\Resources\TestsFetchingResource;
 use Krystal\Katapult\Tests\Concerns\Resources\TestsListingResource;
