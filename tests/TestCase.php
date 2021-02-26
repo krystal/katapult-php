ae3d5dfdce0c32c23f412e07d6eae85fe0a39cca
Josh Bonfield <josh@grizzlyware.com>
2021-02-26 16:39:07 +0000

    test: tidy up tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,16 +7,6 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 abstract class TestCase extends PHPUnitTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-    }
-
     protected function hasTrait(string $trait): bool
     {
         return Helper::classUsesTrait($this, $trait);
